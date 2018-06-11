fb8c37a4609557e40b919076140d9702990cf9bd
Chad Roberts <croberts@redhat.com>
2018-06-11 14:53:24 -0400

    Update docs and sample script for modern extension usage. (#95)
    * Updating to use oc version 3.9 and install the extension.
    
    * Timing tweaks to hopefully make sure the new web console pod comes up.
    
    * Move storedconfig creation to prepare.sh
    
    * Set s2i source reference to PR source when appropriate
    
    * Adding in wait for webconsole readiness.
    
    * Updating oshinko-setup.sh script to install with extension.
    
    * Updating README.md for modern extension documentation and deleting old template.

--- tools/oshinko-setup.sh
+++ tools/oshinko-setup.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-
-while getopts c:fh opt; do
+OPENSHIFT_VERSION=v3.9
+while getopts c:f:i:oh opt; do
     case $opt in
-        c)
-            CONFIG_DIR=$OPTARG
-            ;;
         f)
             FLUSH_IPTABLES="true"
             ;;
         h)
-            echo "Usage: oshinko-setup.sh [-c <directory to use for origin config>] [-f]"
-            echo "Example: oshinko-setup.sh -c /home/myname/originconfig"
-            echo "    results in the cluster configuration being stored in /home/myname/originconfig"
-            echo "If -c is not set, the default config directory will be /etc/originconfig"
+            echo "Usage: oshinko-setup.sh [-f]"
+            echo "Example: oshinko-setup.sh -i 192.168.1.10"
+            echo "If -o is used, it will be the version of openshift used, default is v3.9"
+            echo "If -i is used, the value will be the host used for your openshift cluster"
             echo "If -f is used, 'sudo iptables -F' will be issued before oc cluster up"
             echo "    It may help resolve a potential skydns issue"
             echo ""
             exit
+            ;;
+        i)
+            HOST_IP_ADDRESS=$OPTARG
+            ;;
+        o)
+            OPENSHIFT_VERSION=$OPTARG
             ;;
         \?)
             echo "Invalid option: -$OPTARG" >&2
@@ -26,59 +29,54 @@ while getopts c:fh opt; do
     esac
 done
 
-if [ -z "$CONFIG_DIR" ]
-then
-    echo "CONFIG_DIR not set, using default of /etc/originconfig"
-    CONFIG_DIR="/etc/originconfig"
-fi
-
-# install some stuff we need for building
-rpm -qa | grep -qw git || sudo dnf -y install git
-rpm -qa | grep -qw docker || sudo dnf -y install docker
-rpm -qa | grep -qw wget || sudo dnf -y install wget
-rpm -qa | grep -qw tar || sudo dnf -y install tar
-
-############ get the oshinko repos and build the images
-
-sudo systemctl start docker 
-
-CURRDIR=`pwd`
-export GOPATH=$CURRDIR/oshinko
-
-SRCDIR=$CURRDIR/oshinko/src/github.com/radanalyticsio
-mkdir -p $SRCDIR
-cd $SRCDIR
-if [ ! -d "oshinko-console" ]; then
-    git clone http://github.com/radanalyticsio/oshinko-console
-fi
-
-########### get the origin image and run oc cluster up
-########### this part can be replaced with some other openshift install recipe
 
 
-cd $CURRDIR
-ORIGIN_VERSION=v1.4.0-rc1
-ORIGIN_TARBALL=openshift-origin-client-tools-v1.4.0-rc1.b4e0954-linux-64bit.tar.gz
-ORIGIN_DIR=openshift-origin-client-tools-v1.4.0-rc1+b4e0954-linux-64bit
+function setup_openshift() {
+  sudo docker cp $(docker create docker.io/openshift/origin:$OPENSHIFT_VERSION):/bin/oc /usr/local/bin/oc
+  oc cluster up --version=$OPENSHIFT_VERSION --public-hostname=$HOST_IP_ADDRESS
+  docker pull docker.io/radanalyticsio/openshift-spark
+  oc login -u system:admin
+  export REGISTRY_URL=$(oc get svc -n default docker-registry -o jsonpath='{.spec.clusterIP}:{.spec.ports[0].port}')
+  oc login -u developer -p developer
+}
 
-if [ ! -d "$ORIGIN_DIR" ]; then
-    wget https://github.com/openshift/origin/releases/download/$ORIGIN_VERSION/$ORIGIN_TARBALL
-    tar -xvzf $ORIGIN_TARBALL
-    sudo cp ${ORIGIN_DIR}/* /usr/bin
-fi
+function install_extension() {
+  export SOURCE_TREE="radanalyticsio/oshinko-console"
+  oc login -u system:admin
+  oc new-project oshinko-console
+  oc new-app centos/httpd-24-centos7~https://github.com/${SOURCE_TREE} --context-dir=dist
+  sleep 30s
+  oc create route edge --service oshinko-console
+  sleep 2s
+  export CONSOLE_BASE_ROUTE=$(oc get route oshinko-console --template={{.spec.host}})
+  # change the console config to reference our extension
+  oc project openshift-web-console
+  oc get configmap webconsole-config -o yaml > webconfig.yaml
+  sed -i "s#scriptURLs\: \[\]#scriptURLs\:\n        - https://$CONSOLE_BASE_ROUTE/scripts/scripts.js\n        - https://$CONSOLE_BASE_ROUTE/scripts/templates.js#" webconfig.yaml
+  sed -i "s#stylesheetURLs: \[\]#stylesheetURLs\:\n        - https://$CONSOLE_BASE_ROUTE/styles/oshinko.css\n#" webconfig.yaml
+  oc replace -f webconfig.yaml
+  echo "Here is the updated config for webconsole"
+  oc get configmap webconsole-config -o yaml
+  #restart web console
+  oc project openshift-web-console
+  export OLD_CONSOLE_POD=$(oc get pods -l webconsole=true --template="{{range .items}}{{.metadata.name}}{{end}}")
+  oc delete pod $OLD_CONSOLE_POD
+  sleep 20s
+  export NEW_CONSOLE_POD=$(oc get pods -l webconsole=true --template="{{range .items}}{{.metadata.name}}{{end}}")
+  oc logs $NEW_CONSOLE_POD
+  echo "Listing of all pods"
+  oc get pods --all-namespaces
+}
 
-sudo sed -i -e "/^# INSECURE_REGISTRY/{ s/.*/INSECURE_REGISTRY='--insecure-registry 172.30.0.0\/16'/ }" /etc/sysconfig/docker
-sudo systemctl restart docker
+function setup_docker_insecure_registry() {
+  rpm -qa | grep -qw git || sudo dnf -y install git
+  rpm -qa | grep -qw docker || sudo dnf -y install docker
+  sudo systemctl start docker
+  sudo sed -i -e "/^# INSECURE_REGISTRY/{ s/.*/INSECURE_REGISTRY='--insecure-registry 172.30.0.0\/16'/ }" /etc/sysconfig/docker
+  sudo systemctl restart docker
+}
 
-# make sure your local host name can be resolved!
-# put it in /etc/hosts if you have to, otherwise you will have no nodes
-sudo oc cluster up --host-config-dir=$CONFIG_DIR
-sudo oc cluster down
-sudo cp $SRCDIR/oshinko-console/dist/scripts/*.js $CONFIG_DIR/master
-sudo cp $SRCDIR/oshinko-console/dist/styles/*.css $CONFIG_DIR/master
-sudo sed -i -e "s/extensionDevelopment: false/extensionDevelopment: true/" $CONFIG_DIR/master/master-config.yaml
-sudo sed -i -e "s/extensionScripts: null/extensionScripts:\n    - templates.js\n    - scripts.js/" $CONFIG_DIR/master/master-config.yaml
-sudo sed -i -e "s/extensionStylesheets: null/extensionStylesheets:\n    - oshinko.css/" $CONFIG_DIR/master/master-config.yaml
+
 if [ "$FLUSH_IPTABLES" == "true" ]
 then
     echo "Flushing IP tables"
@@ -86,15 +84,7 @@ then
 else
     echo "Not flushing IP tables"
 fi
-sudo oc cluster up --host-config-dir=$CONFIG_DIR --use-existing-config
 
-############
-
-# Push to a default oshinko project for a default oshinko user
-oc login -u developer -p dev
-oc new-project oshinko
-
-# set up the oshinko service account
-oc create sa oshinko                          # note, VV, first oshinko is the proj name :)
-oc policy add-role-to-user edit system:serviceaccount:developer:oshinko -n oshinko
-
+setup_docker_insecure_registry
+setup_openshift
+install_extension
