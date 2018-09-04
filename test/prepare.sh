c287045d382349f75f9ded838cc263cafd1eb239
Chad Roberts <croberts@redhat.com>
2018-09-04 13:11:53 -0400

    Updating travis-ci to use openshift v3.10 for testing.

--- test/prepare.sh
+++ test/prepare.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-export OPENSHIFT_VERSION=v3.9
+export OPENSHIFT_VERSION=v3.10
 
 function prepare() {
   ip addr show eth0
   export HOST_IP_ADDRESS="$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)"
   echo "Host IP is $HOST_IP_ADDRESS"
   sudo docker cp $(docker create docker.io/openshift/origin:$OPENSHIFT_VERSION):/bin/oc /usr/local/bin/oc
-  oc cluster up --version=$OPENSHIFT_VERSION --public-hostname=$HOST_IP_ADDRESS
+  oc cluster up --public-hostname=$HOST_IP_ADDRESS
   docker pull docker.io/radanalyticsio/openshift-spark
   oc login -u system:admin
   export REGISTRY_URL=$(oc get svc -n default docker-registry -o jsonpath='{.spec.clusterIP}:{.spec.ports[0].port}')
